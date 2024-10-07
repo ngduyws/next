2f878a52c61ea01add920ac4d23de6a86f732777
ngduyws <49272569+ngduyws@users.noreply.github.com>
2024-10-07 11:56:32 +0700

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,70 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+import { Nunito } from "next/font/google";
+
+const nunito = Nunito({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "PWA with Next 14",
+  description: "PWA application with Next 14",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs14", "next14", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [{ name: "Author" }],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="generator" content={metadata.generator} />
+        <link rel="manifest" href={metadata.manifest} />
+        <meta name="keywords" content={metadata.keywords.join(", ")} />
+        {metadata.themeColor.map(({ media, color }, index) => (
+          <meta key={index} name="theme-color" media={media} content={color} />
+        ))}
+        {metadata.authors.map(({ name, url }, index) => (
+          <meta
+            key={index}
+            name="author"
+            content={name}
+            {...(url && { href: url })}
+          />
+        ))}
+        <meta name="viewport" content={metadata.viewport} />
+        {metadata.icons.map(({ rel, url }, index) => (
+          <link key={index} rel={rel} href={url} />
+        ))}
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
